aa5cd3e5ee407c1930116011185dfc9042b2f184
ayindesegun <ayindesegun17@gmail.com>
2020-07-26 11:46:41 +0100

    Updates

---
+++ precache-manifest.e1af22bf15965fd551704bbe2178aa88.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b99b821c6f3be0fc8f5b137981ed2d9e",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "eaaea3668ddf7ba5720b",
+    "url": "/monsters-rolodex/static/css/main.531af456.chunk.css"
+  },
+  {
+    "revision": "00805d01535403bf0738",
+    "url": "/monsters-rolodex/static/js/2.a9317473.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.a9317473.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eaaea3668ddf7ba5720b",
+    "url": "/monsters-rolodex/static/js/main.0f3ddd73.chunk.js"
+  },
+  {
+    "revision": "e481ec9946181f95c60b",
+    "url": "/monsters-rolodex/static/js/runtime-main.4cfc02da.js"
+  }
+]);
